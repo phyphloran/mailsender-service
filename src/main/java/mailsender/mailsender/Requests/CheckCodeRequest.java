844a5069bb93fef071271f94ddd06a62c401e710
phyphloran <mareeviv@yandex.ru>
2026-01-03 18:38:02 +0300

    updated validation in requests

--- src/main/java/mailsender/mailsender/Requests/CheckCodeRequest.java
+++ src/main/java/mailsender/mailsender/Requests/CheckCodeRequest.java
@@ -3,14 +3,18 @@ package mailsender.mailsender.Requests;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 
 public record CheckCodeRequest(
 
         @Email(message = "Email should be valid")
+        @NotBlank(message = "Email can not be empty")
+        @Size(max = 50, message = "The length of the email should be no more than 50")
         String email,
 
         @NotBlank(message = "Code can not be empty")
+        @Size(min = 6, max = 6, message = "The code must be 6 characters long")
         String code
 
 ) {
--- src/main/java/mailsender/mailsender/Requests/GetCodeRequest.java
+++ src/main/java/mailsender/mailsender/Requests/GetCodeRequest.java
@@ -2,11 +2,15 @@ package mailsender.mailsender.Requests;
 
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 
 public record GetCodeRequest(
 
         @Email(message = "Email should be valid")
+        @NotBlank(message = "Email can not be empty")
+        @Size(max = 50, message = "The length of the email should be no more than 50")
         String email
 
 ) {
